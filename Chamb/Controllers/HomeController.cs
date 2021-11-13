76adc2f38159854c44452b2cfa9210fe0ca8f1cf
user <user@WIN-SLVTVVL38RL>
2021-11-13 15:55:12 +0500

    Add project files.

---
+++ Chamb/Controllers/HomeController.cs
@@ -0,0 +1,35 @@
+﻿
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Chamb.Controllers
+{
+    public class HomeController : Controller
+    {
+       
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult About()
+        {
+            return View();
+        }
+
+        public IActionResult Howitwork()
+        {
+            return View();
+        }
+        public IActionResult Pricing()
+        {
+            return View();
+        }
+    }
+}
